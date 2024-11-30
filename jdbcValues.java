d1912a3744c20b662b43448013bdd99fa56b36fc
nahumguess <ngessesse@luc.edu>
2024-11-29 18:30:25 -0600

    Added database information

--- jdbcValues.java
+++ jdbcValues.java
@@ -1,2 +1,5 @@
 public interface jdbcValues {
+    final String DB_URL = "jdbc:mysql://localhost:3306/softeng_health_club_db";
+    final String DB_USERNAME = "root";
+    final String DB_PASSWORD = "asdf1234";
 }
