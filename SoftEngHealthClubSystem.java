8516cff2566b1ab23afd475d928ffb01854eaf5b
Alina Zacaria <alinazac9@gmail.com>
2024-11-29 23:32:12 -0600

    created sys class

---
+++ SoftEngHealthClubSystem.java
@@ -0,0 +1,14 @@
+public class SoftEngHealthClubSystem {
+
+    public Guest user;
+
+    public SoftEngHealthClubSystem(Guest g) {
+
+        user = g;
+        try {UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");}
+        catch (Exception e) {System.out.println("Look and Feel not set");}
+
+        HealthClubHomePage homePage = new HealthClubHomePage(null, user);
+        homePage.welcomeText.setText("Welcome " + user.getName());
+    }
+}
