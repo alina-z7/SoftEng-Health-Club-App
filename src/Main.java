cd4a592b9460c8a002fbf4c35220816fee8f5f6a
Jai-Fischer <89214477+JaiCantCode@users.noreply.github.com>
2024-12-01 21:26:16 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        LogIn login = new LogIn();
+        Guest guest = login.logIn();
+        SoftEngHealthClubSystem softEngHealthClubSystem = new SoftEngHealthClubSystem(guest);
+    }
+}
